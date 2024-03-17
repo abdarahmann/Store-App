6cf5b7f437dc21a04637d08df7c8db7a742cd494
abdoo <abdoalgamal84@gmail.com>
2024-03-17 21:49:15 +0200

    Remove Some mistakes

--- lib/cubits/update_product_cubit/update_product_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'update_product_state.dart';
-
-class UpdateProductCubit extends Cubit<UpdateProductState> {
-  UpdateProductCubit() : super(UpdateProductInitial());
-  
-}
--- lib/cubits/update_product_cubit/update_product_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'update_product_cubit.dart';
-
-@immutable
-sealed class UpdateProductState {}
-
-final class UpdateProductInitial extends UpdateProductState {}
