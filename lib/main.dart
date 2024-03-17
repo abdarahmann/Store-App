17ece3c4f78dc47e02344a422925b105f85ae557
abdoo <abdoalgamal84@gmail.com>
2024-03-17 21:48:53 +0200

    remove some Mistakes

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ import 'package:store/Screens/add_product_screen.dart';
 import 'package:store/Screens/allcategories_screen.dart';
 import 'package:store/Screens/category_screen.dart';
 import 'package:store/Screens/settings_screen.dart';
-import 'package:store/cubits/all_categories_cubit/all_categories_cubit.dart';
 import 'package:store/cubits/category_cubit/category_cubit.dart';
 import 'Screens/home_page.dart';
 import 'Screens/update_product_screen.dart';
-import 'cubits/all_products_cubit/all_products_cubit.dart';
+import 'cubits/category_cubit/category_cubit/all_products_cubit.dart';
+import 'cubits/category_cubit/category_cubit/cubit/all_categories_cubit.dart';
 import 'simple_bloc_observer.dart';
 
 void main() {
